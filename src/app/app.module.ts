566fa0e59ffbc9dc87d86fe98681f977c5c00909
Meigara Juma <cracker394@gmail.com>
2018-04-03 01:38:34 +0700

    Solving simple scroll page, portfolio part added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { AppRoutingModule } from './app.routing';
 import { MDBBootstrapModule } from 'angular-bootstrap-md';
+import { SimpleSmoothScrollModule } from 'ng2-simple-smooth-scroll';
 
 import { AppComponent } from './app.component';
 import { FooterComponent } from './shared/footer/footer.component';
 import { NavbarComponent } from './shared/navbar/navbar.component';
 import { HomeComponent } from './home/home.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
 
 
 
@@ -16,12 +20,16 @@ import { HomeComponent } from './home/home.component';
     AppComponent,
     FooterComponent,
     NavbarComponent,
-    HomeComponent
+    HomeComponent,
+    PortfolioComponent,
+    AboutComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
     RouterModule,
     MDBBootstrapModule.forRoot(),
+    SimpleSmoothScrollModule,
     AppRoutingModule
   ],
   schemas: [ NO_ERRORS_SCHEMA ],
--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -4,9 +4,15 @@ import { BrowserModule  } from '@angular/platform-browser';
 import { Routes, RouterModule } from '@angular/router';
 
 import { HomeComponent } from './home/home.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
 
 const routes: Routes = [
     { path: 'home', component: HomeComponent },
+    { path: 'portfolio', component: PortfolioComponent },
+    { path: 'about', component: AboutComponent },
+    { path: 'contact', component: ContactComponent },
     { path: '', redirectTo: 'home', pathMatch: 'full' }
 ];
 
